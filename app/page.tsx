4c5867f0f884b7ce5c31b38d2f805304c1adc6ee
Jalen Murray <jalenmmurray@gmail.com>
2024-07-03 18:53:31 -0400

    Added redirect to themes page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  redirect('/themes');
   return (
     <main className="flex min-h-screen w-full flex-col gap-12 p-24 bg-slate-900 text-white">
       Nothing Here! Go to{' '}
